976d726f025b0293d5cd776c3bdfdd7c539743c1
Philipp Resl <resl.philipp@gmail.com>
2020-06-08 10:10:04 +0200

    Add explicit submission system selection

--- submit.sh
+++ submit.sh
@@ -6,9 +6,10 @@ set -e
 usage() {
         echo "Welcome to the pipeline submission script. A script helps to submit jobs to SLURM and SGE clusters with snakemake and singularity"
         echo
-        echo "Usage: $0 [-v] [-c <cluster_config_file>] [-s <snakemke_args>]"
+        echo "Usage: $0 [-v] [-t <cluster_type>] [-c <cluster_config_file>] [-s <snakemke_args>]"
         echo
         echo "Options:"
+	echo "  -t <cluster_type> Specify explicitly which submission system is available. Options: sge, slurm"
         echo "  -c <cluster_config_file> Path to cluster config file in YAML format (mandatory). "
         echo "  -s <snakemake_args> Additional arguments passed on to the snakemake command (optional). snakemake is run with --immediate-submit -pr --notemp --latency-wait 600 --use-singularity --jobs 1001 by default."
 	echo "  -i \"<singularity_args>\" Additional arguments passed on to singularity (optional). Singularity is run with -B /tmp:/usertmp by default."
@@ -19,11 +20,12 @@ version() {
         exit 0
 }
 
-while getopts ":v:c:s:" option;
+while getopts ":v:t:c:s:" option;
         do
                 case "${option}"
                 in
                         v) version;;
+			t) CLUSTER=${OPTARG};;
                         c) CLUSTER_CONFIG=${OPTARG};;
                         s) SM_ARGS=${OPTARG};;
 			i) SI_ARGS=${OPTARG};;
@@ -33,14 +35,20 @@ while getopts ":v:c:s:" option;
         done
 if [ $OPTIND -eq 1 ]; then usage; fi
 
-CLUSTER=""
-command -v qsub >/dev/null 2>&1 && { echo >&2 "SGE detected, will use qsub to submit jobs."; CLUSTER="sge"; }
-command -v sbatch >/dev/null 2>&1 && { echo >&2 "SLURM detected, will use sbatch to submit jobs."; CLUSTER="slurm"; }
+# Testing warning
+echo "WARNING: The submission script is still in testing mode! Check the snakemake command inside before moving into production!"
 
-
-echo "the submission script is in testing mode! Check the snakemake command inside before moving into production!"
-
-
+# Determine submission system:
+if [ $CLUSTER = "sge" ]; then
+	echo "SGE (Sun Grid Engine) submission system specified. Will use qsub to submit jobs."
+elif [ $CLUSTER = "slurm" ]; then
+	echo "SLURM submission system specified. Will use sbatch to submit jobs."
+else
+	echo "No or unknown submission system specified, will try to detect the system automatically."
+	CLUSTER=""
+	command -v qsub >/dev/null 2>&1 && { echo >&2 "SGE detected, will use qsub to submit jobs."; CLUSTER="sge"; }
+	command -v sbatch >/dev/null 2>&1 && { echo >&2 "SLURM detected, will use sbatch to submit jobs."; CLUSTER="slurm"; }
+fi
 
 if [ $CLUSTER = "slurm" ]; then
         export CONDA_PKGS_DIRS="$(pwd)/.conda_pkg_tmp"
